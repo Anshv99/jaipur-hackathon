d0bc9c95c4d894c51eb1749750e2b6352591cbb9
bored-ace-2003 <68457186+bored-ace-2003@users.noreply.github.com>
2023-03-20 04:11:15 +0000

    fix for vite on codespaces

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { hmr: { clientPort: process.env.CODESPACES ? 443 : undefined } },
   define: {
     'process.env': process.env
   },
